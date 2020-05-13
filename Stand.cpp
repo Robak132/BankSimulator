346aad5a06f1552fa41d426571ba5892d8e3069f
Oskar Mateusz Bartosz <o.bartosz@stud.elka.pw.edu.pl>
2020-05-13 19:13:53 +0200

    Upload New File

---
+++ Stand.cpp
@@ -0,0 +1,36 @@
+﻿// Stand.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include "Stand.h"
+
+using namespace std;
+
+Stand::Stand(ID stand_id)
+{
+    self_ID = stand_id;
+    /*static vector<string> names{ "WithdrawMoney", "DepositMoney", "TransferMoney",
+        "CreateAccount", "AccountInfo", "DeleteAccount()", "TakeLoan", "RepayLoan" };
+    static vector<Operation> operations{ WithdrawMoney obj1, };*/
+}
+
+ID Stand::getClientID()
+{
+    return client_ID;
+}
+
+ID Stand::getStandID()
+{
+    return self_ID;
+}
+
+ID Stand::getEmployeetID()
+{
+    return employeet_ID;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
