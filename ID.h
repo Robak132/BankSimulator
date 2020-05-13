b1bfed7a7ce4140d89a80d76a272d28c5135c35f
Jakub Robaczewski <j.robaczewski@stud.elka.pw.edu.pl>
2020-05-13 08:10:28 +0200

    auto id (status int)

--- ID.h
+++ ID.h
@@ -7,13 +7,15 @@ using namespace std;
 class ID {
 private:
 	string id_number;
-	bool CheckID(string _ID);
+	bool checkID(string _id);
+	string extendID(string _id);
 public:
 	ID(string _id_number="000000");
 	string getID();
-	void setID(string _ID);
+	void setID(string _id);
+	void setNewID();
 };
-
+ostream& operator << (ostream& out, ID& id);
 bool operator == (ID a, ID b);
 bool operator == (ID a, string b);
 bool operator != (ID a, ID b);
