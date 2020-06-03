55517bb45003423d892eea115559269ff6f819bf
Jakub Robaczewski <j.robaczewski@stud.elka.pw.edu.pl>
2020-06-03 23:22:47 +0200

    Komentarzy moc

--- Timer.cpp
+++ Timer.cpp
@@ -1,7 +1,6 @@
 #include "Timer.h"
 #include "Tools.h"
 
-
 using namespace std;
 
 Timer::Timer(int _time_per_tick, string _log, BankSetup _bs, int st) {
@@ -11,7 +10,7 @@ Timer::Timer(int _time_per_tick, string _log, BankSetup _bs, int st) {
 	actual_time = bank->getOpenTime();
 
 	try {
-		log.open(_log);
+		log.open(_log);	// Otwarcie logu
 	}
 	catch (exception ex) {
 		throw FileNotFound("Log file not found");
@@ -19,17 +18,17 @@ Timer::Timer(int _time_per_tick, string _log, BankSetup _bs, int st) {
 }
 Timer::~Timer() {
 	delete bank;
-	log.close();
+	log.close();		// Zamkniêcie logu
 }
 
 void Timer::runSimulation() {
 	log << "Simulation started" << endl;
 	while (bank->getCloseTime() > actual_time) {
 		bool not_timed = true;
-		if (Tools::randomInt() % 5 == 0) {
+		if (Tools::randomInt() % 5 == 0) {			// Losuje klienta - szansa = 20% * (iloœæ klientów poza bankiem/iloœæ zapisanych klientów)
 			IClient* temp_client = bank->randomClient();
 			if (!temp_client->inBank()) {
-				clients_in.push_back(temp_client);
+				clients_in.push_back(temp_client);	// Dodaje klienta do vektora klientów w banku
 				if (not_timed)
 				{
 					log << endl;
@@ -39,15 +38,15 @@ void Timer::runSimulation() {
 				}
 				log << "A wild client appeared: " << temp_client << "." << endl;
 				cout << "A wild client appeared: " << temp_client << "." << endl;
-				bank->addClientToList(temp_client);
+				bank->addClientToList(temp_client);	// Dodaje klienta do kolejki
 				log << temp_client->getNameSurname() << " [" << temp_client->getIDNumber() << "] chose " << temp_client->getCurrentStand() << "." << endl;
 				cout << temp_client->getNameSurname() << " [" << temp_client->getIDNumber() << "] chose " << temp_client->getCurrentStand() << "." << endl;
 			}
 		}
-		bank->iterateThrough();
+		bank->iterateThrough();						// Przechodzi przez wszystkie okienka, zmniejsza czasy i wpuszcza nowych klientów
 		for (int i = clients_in.size()-1 ; i>=0; i--)
 		{
-			if (!clients_in[i]->inBank())
+			if (!clients_in[i]->inBank())			// Usuniêcie klienta, który wyszed³ z banku
 			{
 				if (not_timed)
 				{
@@ -66,29 +65,28 @@ void Timer::runSimulation() {
 					log << clients_in[i] << " finished doing it with " << clients_in[i]->getCurrentStand()->getSType() << " [" << clients_in[i]->getCurrentStand()->getStandID().getID() << "]." << endl;
 					cout << clients_in[i] << " finished doing it with " << clients_in[i]->getCurrentStand()->getSType() << " [" << clients_in[i]->getCurrentStand()->getStandID().getID() << "]." << endl;
 				}
-					clients_in.erase(clients_in.begin()+i);
+				clients_in.erase(clients_in.begin()+i);
 			}
 
 		}
 		actual_time += time_per_tick;
 		if (!not_timed)
 		{
-			if (sleep_time != 0)
-				this_thread::sleep_for(chrono::seconds(sleep_time));
+			this_thread::sleep_for(chrono::seconds(sleep_time));
 			not_timed = true;
 		}
 	}
 
 	log << endl << getFormatedTime() << endl;
 	cout << endl << getFormatedTime() << endl;
-	for (int i = clients_in.size() - 1; i >= 0; i--) {
+	for (int i = clients_in.size() - 1; i >= 0; i--) { // Wypisanie klientów, którym nie uda³o siê ukoñczyæ operacji
 		log << clients_in[i] << " failed doing do it because simulation ended." << endl;
 		cout << clients_in[i] << " failed doing do it because simulation ended." << endl;
 		clients_in.erase(clients_in.begin() + i);
 	}
 	log << "Simulation ended" << endl;
 	cout << "Simulation ended" << endl;
-	bank->getStats().saveIt();
+	bank->getStats().saveIt();	// Zapisanie statystyk
 }
 
 
@@ -111,8 +109,4 @@ string Timer::getFormatedTime() {
 		hours = "0" + hours;
 	string time = hours + ":" + minutes;
 	return time;
-}
-
-void Timer::setTimePerTick(int _time_per_tick) {
-	time_per_tick = _time_per_tick;
 }
